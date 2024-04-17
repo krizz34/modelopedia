523035e9145f06483c8f4040fabdc9d58de2cb09
Anand <panandakrishna@gmail.com>
2024-04-17 21:49:41 +0530

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer mt-auto py-3 bg-dark">
     <div class="container px-3">
         <div class="row">
-            <div class="col-lg-3 col-md-12">
+            <!-- <div class="col-lg-3 col-md-12">
                 <h5 class="text-white fw-bold fs-6">WE ARE SOCIAL</h5>
                 <ul class="list-unstyled">
                     <div class="row">
@@ -11,31 +11,90 @@
                         <div class="col"><li><a href="#" class="circle"><i class="fab fa-twitter"></i></a></li></div>
                     </div>    
                 </ul>
-            </div>
+            </div> -->
             <div class="col-lg-3 col-md-6">
-                <h5 class="text-white fw-bold">VISIT US</h5>
+                <h5 class="text-white fs-6">MODELOPEDIA</h5>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <h5 class="text-white fs-6">MENU</h5><br/>
+                <ul class="list-unstyled">
+                    <li><a href="#" style="text-decoration: none; color:white">About</a></li><br/>
+                    <li><a href="#" style="text-decoration: none; color:white">Blog</a></li><br/>
+                    <li><a href="#" style="text-decoration: none; color:white">Contest</a></li><br/>
+                    <li><a href="#" style="text-decoration: none; color:white">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white fs-6">HELP</h5><br/>
+                <div class="row">
+                    <div class="col-6">
+                        <ul class="list-unstyled">
+                            <li><a href="#" style="text-decoration: none; color:white">Testimony</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">How it works?</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">Terms & Conditions</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <ul class="list-unstyled">
+                            <li><a href="#" style="text-decoration: none; color:white">FAQs</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">Refund Policy</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">Our Team</a></li><br/>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-lg-3 col-md-12">
+                <h5 class="text-white fw-bold">Have a Question? </h5>
+                <ul class="list-unstyled">
+                    <li><i class="fab fa-location text-white"></i> Address</li>
+                    <li><i class="fab fa-phone" style="color: white;"></i> Phone Number</li>
+                    <li><i class="fab fa-envelope" style="color: white;"></i> Email Address</li>
+                </ul>
+            </div> -->
+            <!-- <div class="col-lg-3 col-md-12">
+                <h5 class="text-white fw-bold" style="font-size: 18px">Contact Us</h5>
+                <ul class="list-unstyled contact-info">
+                    <li><i class="fab fa-location" style="color: white;"></i> Address</li>
+                    <li><i class="fab fa-phone" style="color: white;"></i> Phone Number</li>
+                    <li><i class="fab fa-envelope" style="color: white;"></i> Email Address</li>
+                </ul>
+            </div> -->
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-white fs-6">HAVE A QUESTION?</h5><br/>
+                <div class="row">
+                    <div class="col-2">
+                        <ul class="list-unstyled">
+                            <li><i class="fas fa-map-marker-alt" style="color: white;"></i></li><br/><br/><br/><br/>
+                            <li><i class="fas fa-phone" style="color: white;"></i></li><br/>
+                            <li><i class="fas fa-envelope" style="color: white;"></i></li>
+                        </ul>
+                    </div>
+                    <div class="col-10">
+                        <ul class="list-unstyled">
+                            <li><a href="#" style="text-decoration: none; color:white">Suite 112, 39/2475-B1, LR Towers, Sjrra 104, Janatha Rd, Palarivattom, Cochin, Kerala - 682 025</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">+91 9207094607</a></li><br/>
+                            <li><a href="#" style="text-decoration: none; color:white">sales@modelopedia.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-lg-3 col-md-6">
+                <h5 class="text-white fs-6">Company</h5>
                 <ul class="list-unstyled">
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Contact Us</a></li>
                     <li><a href="#">Careers</a></li>
                 </ul>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white">Company</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Careers</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white">Services</h5>
+            </div> -->
+            <!-- <div class="col-lg-3 col-md-6">
+                <h5 class="text-white fs-6">Services</h5>
                 <ul class="list-unstyled">
                     <li><a href="#">Web Development</a></li>
                     <li><a href="#">Mobile App Development</a></li>
                     <li><a href="#">UI/UX Design</a></li>
                 </ul>
-            </div>
+            </div> -->
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
